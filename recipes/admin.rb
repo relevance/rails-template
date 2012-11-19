20bc98b1bebe5d4d6a28a43c6fa91b207b1f874e
Daemian Mack and Lake Denman <pair+dm+ld@thinkrelevance.com>
2012-11-19 13:42:12 -0500

    Add git commit routine to admin recipe.

--- recipes/admin.rb
+++ recipes/admin.rb
@@ -6,6 +6,10 @@ if prefs[:admin]
 
   after_bundler do
     generate "active_admin:install"
+
+    ### GIT ###
+    git :add => '-A' if prefer :git, true
+    git :commit => '-qm "rails_apps_composer: active_admin"' if prefer :git, true
   end
 end
 
